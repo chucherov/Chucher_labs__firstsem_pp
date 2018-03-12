0189969f8d3cdca81388e3c3d43565ad2ad42d44
chucherov <31537725+chucherov@users.noreply.github.com>
2018-03-12 19:43:13 +0300

    Update Temperature.c

--- Temperature.c
+++ Temperature.c
@@ -1,18 +1,28 @@
-#include <stdio.h> 
-int main(int argc, char *argv[]) { 
-   float temp; 
-   char m; 
-   printf("Enter you temperature\n"); 
-   scanf("%f %c", &temp,&m); 
-  switch (m) 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+   double temp=atof(argv[1]);
+   char c;
+   if(argc==2) {
+   c='\0';
+   }
+   else c=*argv[2];
+
+  switch (c)
         { 
-        case 'c': if (temp <= -273.15) 
+        default: {
+		    printf("eror, try again");
+			break;
+		}
+        case 'c': if(temp <= -273.15) {
                   printf("Unable to convert as temperature is equal to absolute zero\n"); 
+                  }
                   else 
                   { 
                   printf("C %.2f\n", temp); 
                   printf("F %.2f\n", 1.8 * temp + 32); 
-                  printf("K %.2f\n", temp + 273.15);} 
+                  printf("K %.2f\n", temp + 273.15);}
                   break;
         case 'f': if (temp <= -459.67) 
                   printf("Unable to convert as temperature is equal to absolute zero\n"); 
@@ -30,7 +40,36 @@ int main(int argc, char *argv[]) {
                  printf("F %.2f\n", 9 * (temp - 273.15)/5.0 + 32); 
                  printf("K %.2f\n", temp);} 
                  break;
-                 default: printf("eror, try again"); 
+        case '\0': {
+	if (temp <= -273.15) {
+                printf("Unable to convert as temperature is equal to absolute zero\n"); 
+                }
+                else 
+                { 
+                printf("C %.2f\n", temp); 
+                printf("F %.2f\n", 1.8 * temp + 32); 
+                printf("K %.2f\n", temp + 273.15);} 
+                printf("\n");
+                if (temp <= -459.67) 
+                  printf("Unable to convert as temperature is equal to absolute zero\n"); 
+                  else 
+                  { 
+                  printf("C %.2f\n", 5.0/9.0*(temp-32)); 
+                  printf("F %.2f\n", temp); 
+                  printf("K %.2f\n",5 * (temp - 32) + 273.15);}
+                  printf("\n");
+                if (temp <= 0) 
+                  printf("Unable to convert as temperature is equal to absolute zero\n"); 
+                 else 
+                 { 
+                 printf("C %.2f\n", temp - 273.15); 
+                 printf("F %.2f\n", 9 * (temp - 273.15)/5.0 + 32); 
+                 printf("K %.2f\n", temp);}
+				 printf("\n"); 
+                break;
+              }
+         
                  } 
+    
         return 0; 
 }
